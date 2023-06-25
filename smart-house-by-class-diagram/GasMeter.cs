8c284ca1cbc75ba9d36e2d30559c01aa72c0983b
Stjepan Petrović <stjepanpetrovic11@gmail.com>
2023-06-25 23:39:02 +0200

    Add GasMeter class which inherits abstract class Meter

---
+++ smart-house-by-class-diagram/GasMeter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace smart_house_by_class_diagram
+{
+    internal class GasMeter : Meter
+    {
+        public new string Name { get; set; }
+        public new string Unit { get; set; }
+        protected new string RawData { get; set; }
+
+        public GasMeter(string name, string unit, string rawData) : base(name, unit, rawData)
+        {
+            Name = name;
+            Unit = unit;
+            RawData = rawData;
+        }
+
+        public override double[] GetDailyData()
+        {
+            string[] dailyDataString = RawData.Split(';');
+            double[] dailyDataDouble = null;
+            int counter = 0;
+
+            foreach (var item in dailyDataString)
+            {
+                dailyDataDouble[counter] = double.Parse(item) / 1000;
+
+                counter++;
+            }
+
+            return dailyDataDouble;
+        }
+    }
+}
