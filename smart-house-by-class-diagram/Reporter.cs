648307591412f84bf1f30711f07467340969b1af
Stjepan Petrović <stjepanpetrovic11@gmail.com>
2023-06-25 23:42:44 +0200

    Add CreateDailySumReport class

---
+++ smart-house-by-class-diagram/Reporter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace smart_house_by_class_diagram
+{
+    internal class Reporter
+    {
+        public string CreateDailySumReport(Meter meter)
+        {
+            if (meter.Unit == "kWh")
+            {
+                return "Daily total value is " + meter.CalculateDailySum() + " kWh";
+            } 
+            else if (meter.Unit == "m3")
+            {
+                return "Daily total value is " + meter.CalculateDailySum() + " m3";
+            }
+
+            return "Unit not found";
+        }
+    }
+}
