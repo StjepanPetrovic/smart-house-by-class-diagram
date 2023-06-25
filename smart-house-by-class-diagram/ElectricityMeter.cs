dfdd2afcf9755c0f7ebd255a9728880fb8d35a83
Stjepan Petrović <stjepanpetrovic11@gmail.com>
2023-06-25 23:35:24 +0200

    Add ElectricityMeter class which inherits abstract class Meter

---
+++ smart-house-by-class-diagram/ElectricityMeter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace smart_house_by_class_diagram
+{
+    internal class ElectricityMeter : Meter
+    {
+        public new string Name { get; set; }
+        public new string Unit { get; set; }
+        protected new string RawData { get; set; }
+
+        public ElectricityMeter(string name, string unit, string rawData) :base(name, unit, rawData)
+        {
+            Name = name;
+            Unit = unit;
+            RawData = rawData;
+        }
+
+        public override double[] GetDailyData()
+        {
+            string[] dailyDataString = RawData.Split(';');
+            double[] dailyDataDouble = null;
+            int counter = 0;
+            
+            foreach (var item in dailyDataString)
+            {
+                dailyDataDouble[counter] = double.Parse(item);
+
+                counter++;
+            }
+
+            return dailyDataDouble;
+        }
+    }
+}
