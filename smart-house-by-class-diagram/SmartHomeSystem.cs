84c7a13b6c2f1d31a94d6c6097ee3ad919a29909
Stjepan Petrović <stjepanpetrovic11@gmail.com>
2023-06-25 23:52:31 +0200

    Add SmartHomeSystem class

---
+++ smart-house-by-class-diagram/SmartHomeSystem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace smart_house_by_class_diagram
+{
+    internal class SmartHomeSystem
+    {
+        public List<Meter> Meters { get; set; } = new List<Meter>();
+
+        public string GetReports()
+        {
+            Reporter reporter = new Reporter();
+            string report = "";
+
+            foreach (Meter meter in Meters)
+            {
+               report += reporter.CreateDailySumReport(meter);
+            }
+
+            return report;
+        }
+    }
+}
