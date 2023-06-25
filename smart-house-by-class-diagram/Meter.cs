2d2fa21104a028c92899b46ce8e275d996a1daeb
Stjepan Petrović <stjepanpetrovic11@gmail.com>
2023-06-25 23:07:04 +0200

    Add abstract class Meter

---
+++ smart-house-by-class-diagram/Meter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace smart_house_by_class_diagram
+{
+    public abstract class Meter
+    {
+        public string Name { get; set; }
+        public string Unit { get; set; }
+        protected string RawData { get; set; }
+
+        public Meter(string name, string unit, string rawData)
+        {
+            Name = name;
+            Unit = unit;
+            RawData = rawData;
+        }
+
+        public abstract double[] GetDailyData();
+
+        public double CalculateDailySum()
+        {
+            double sum = 0;
+
+            foreach (var item in GetDailyData())
+            {
+                sum += item;
+            }
+
+            return sum;
+        }
+    }
+}
